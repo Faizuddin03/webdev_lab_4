748ce9bcdca3bffe0e1725627d52b73f17bcb9dc
Faizuddin Shah Bin Roslay <faizuddinroslay5@gmail.com>
2024-11-24 09:09:27 +0800

    Lab 4

---
+++ test.js
@@ -0,0 +1,36 @@
+// question 1
+function changeContent() {
+    document.getElementById("myElement").textContent = "Hello there!";
+}
+
+// question 2
+function checkInput() {
+    const input = document.getElementById("userInput").value;
+      const number = Number(input);
+
+      if (number < 10) {
+        alert("The number is lower than 10");
+      } 
+      else {
+        alert("The number is higher than 10");
+      }
+}
+
+// question 3
+let text = "";
+    for (let i = 1; i <= 5; i++) { 
+      text += "The number is " + i + "<br>"; 
+}
+    
+document.getElementById("demo").innerHTML = text;
+
+// question 4
+function toggleColor() {
+    const colorBox = document.getElementById("colorBox");
+    if (colorBox.style.backgroundColor === "red") {
+      colorBox.style.backgroundColor = "blue";
+    } 
+    else {
+      colorBox.style.backgroundColor = "red";
+    }
+}
